67609ed000470fc10315b86eddccef4895c078bb
macbook <macbook@localhost>
2022-08-24 11:12:04 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/selenium/drivers/strategies/Chrome.java
@@ -0,0 +1,19 @@
+//package com.juaracoding.selenium.drivers.strategies;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//
+//public class Chrome implements DriverStrategy {
+//	public WebDriver setStraregy() {
+//		WebDriverManager.chromedriver().setup();
+//		ChromeOptions options = new ChromeOptions();
+//		options.setExperimentalOption("useAutomationExtension", false);
+//		options.addArguments("--no-sandbox");
+//
+//		return new ChromeDriver(options);
+//	}
+//
+//}
---
+++ src/main/java/com/juaracoding/selenium/drivers/strategies/DriverStrategy.java
@@ -0,0 +1,9 @@
+package com.juaracoding.selenium.drivers.strategies;
+
+import org.openqa.selenium.WebDriver;
+
+public interface DriverStrategy {
+	
+	WebDriver setStrategy();
+
+}
